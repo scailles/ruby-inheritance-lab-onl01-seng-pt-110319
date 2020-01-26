946065674d9b1f33653ba28c21c4afea274cbbb5
Shaina C <scailles23@gmail.com>
2020-01-26 07:50:24 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+
+def initialize
+  @knowledge=[]
+end
+
+def learn(knowledge)
+ @knowledge << "Ruby framework Rails gem bundle update" 
+end
+
+def knowledge
+  return @knowledge
+end
 
 end
